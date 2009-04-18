a6779b6ab109b9154ae91b20664d0a9a167a5f45
videowhisper <videowhisper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-18 13:50:18 +0000

    Buidling.
    git-svn-id: https://plugins.svn.wordpress.org/videowhisper-video-conference-integration@111522 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- videowhisper_conference/vc_roomc.php
+++
@@ -1,3 +0,0 @@
-<?
-//echo "INSERT INTO `#__vw_rooms` (`name` ,`details` ,`type` ,`owner` ,`capacity` ,`lobby` ,`rank` ,`expires`) VALUES ('".JRequest::getVar( 'rname' )."', '".JRequest::getVar( 'rdescription' )."', '1', '$username', '".JRequest::getVar( 'rcapacity' )."', '', '0', '".(JRequest::getVar( 'rexpires' )*3600*24)."')"; 
-?>
