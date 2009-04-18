3cb2020f874fa10f35d9f3d9c3c6358885fa2ed2
videowhisper <videowhisper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-18 14:53:54 +0000

    Update.
    git-svn-id: https://plugins.svn.wordpress.org/videowhisper-video-conference-integration@111546 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.0.2/videowhisper_conference/vc_login.php
@@ -0,0 +1,13 @@
+﻿<?
+include("../wp-config.php");
+
+get_currentuserinfo();
+
+$loggedin=$msg="";
+if ($userdata->user_nicename)
+$loggedin=1;
+else $msg=urlencode("<a href=\"/\">Please login first or register an account if you don't have one! Click here to return to website.</a>");
+
+$username=urlencode($userdata->user_nicename);
+include("inc.php");
+?>server=<?=$rtmp_server?>&room=Lobby&welcome=Welcome!&username=<?=$username?>&webserver=&msg=<?=$msg?>&tutorial=1&room_delete=0&room_create=0&file_upload=1&file_delete=1&loggedin=<?=$loggedin?>&loadstatus=1
