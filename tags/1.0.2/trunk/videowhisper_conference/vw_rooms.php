d19fddfb98f3a7c4af3f96ef2ff891a9f035233b
videowhisper <videowhisper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-18 13:59:07 +0000

    Release 1.0.2
    git-svn-id: https://plugins.svn.wordpress.org/videowhisper-video-conference-integration@111525 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.0.2/trunk/videowhisper_conference/vw_rooms.php
@@ -0,0 +1,7 @@
+<rooms>
+<room room_name="Lobby" room_description="Welcome!" user_number="0" capacity="100" />
+<room room_name="Fun" room_description="Haha!" user_number="0" capacity="50" />
+<room room_name="Hangout" room_description="Chill..." user_number="0" capacity="50" />
+<room room_name="Room 1" room_description="Some room" user_number="0" capacity="50" />
+<room room_name="Room 2" room_description="Some other room" user_number="0" capacity="50" />
+</rooms>
